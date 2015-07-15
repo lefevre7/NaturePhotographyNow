581fcd73d0813c884343f12b565689440e6c2dfc
Jeremy <lefevre_jeremy@yahoo.com>
2015-07-14 21:46:52 -0600

    Tried to add text and a picture (currently in a playlist format example) to the listview in GalleryActivity. Along with this came the gradient_bg.xml, gradient_bg_hover.xml, list_selector.xml, and image_bg.xml in the drawable folder; list_row.xml in the layout folder; and CustomizedListView.java (which is actually supposed to be the GalleryAcivity--and now GalleryActivity is modified with that code-so it's where it's supposed to be), FileCache.java, ImageLoader.java, LazyAdapter.java, MemoryCache.java, Utils.java, and XMLParser.java. Also, on the previous commit, Androidmanifest was changed--I added the screenorientation to activity_main2--which I also created to try go to a different activity when the phone orientation changed--it layed everything out correctly, but when I clicked on it it was not connected to anything/didn't do anything.

--- app/src/main/java/com/example/jeremy/naturephotographynow/activity/GalleryActivity.java
+++ app/src/main/java/com/example/jeremy/naturephotographynow/activity/GalleryActivity.java
@@ -16,6 +16,13 @@ import android.widget.Toast;
 
 import com.example.jeremy.naturephotographynow.R;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Creates (will obtain) a list (that will be images, but right now is an example)
  * and puts them into an adapter to display
@@ -25,7 +32,20 @@ public class GalleryActivity extends ActionBarActivity {
     /** A tag for logging purposes */
     public static final String GalTAG = "GalleryActivityTag";
 
-    ListView listView;
+    // All static variables
+    static final String URL = "http://api.androidhive.info/music/music.xml";
+    // XML node keys
+    static final String KEY_SONG = "song"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_TITLE = "title";
+    static final String KEY_ARTIST = "artist";
+    static final String KEY_DURATION = "duration";
+    static final String KEY_THUMB_URL = "thumb_url";
+
+    ListView list;
+    LazyAdapter adapter;
+
+    //ListView listView;
 
     /**
      * Creates (will obtain) a list (that will be images, but right now is an example)
@@ -38,6 +58,36 @@ public class GalleryActivity extends ActionBarActivity {
 
         // Get ListView object from xml
         setContentView(R.layout.activity_gallery);
+
+        ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+
+        XMLParser parser = new XMLParser();
+        String xml = parser.getXmlFromUrl(URL); // getting XML from URL
+        Document doc = parser.getDomElement(xml); // getting DOM element
+
+        NodeList nl = doc.getElementsByTagName(KEY_SONG);
+        // looping through all song nodes <song>
+        for (int I = 0; I > nl.getLength(); I++) {
+            // creating new HashMap
+            HashMap<String, String> map = new HashMap<String, String>();
+            Element e = (Element) nl.item(I);
+            // adding each child node to HashMap key => value
+            map.put(KEY_ID, parser.getValue(e, KEY_ID));
+            map.put(KEY_TITLE, parser.getValue(e, KEY_TITLE));
+            map.put(KEY_ARTIST, parser.getValue(e, KEY_ARTIST));
+            map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
+            map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+
+            // adding HashList to ArrayList
+            songsList.add(map);
+        }
+
+        list=(ListView)findViewById(R.id.list);
+
+        // Getting adapter by passing xml data ArrayList
+        adapter=new LazyAdapter(this, songsList);
+
+        /*
 
         listView = (ListView) findViewById(R.id.list);
 
@@ -78,7 +128,13 @@ public class GalleryActivity extends ActionBarActivity {
         listView.setAdapter(adapter);
 
         // ListView Item Click Listener
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {*/
+        // Assign adapter to ListView
+        list.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
 
             /**
              * When clicked on, the user is taken to an individual gallery.
@@ -94,8 +150,11 @@ public class GalleryActivity extends ActionBarActivity {
                 // ListView Clicked item index
                 int itemPosition     = position;
 
+                /*// ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);*/
+
                 // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
+                String  itemValue    = (String) list.getItemAtPosition(position);
 
                 // Show Alert
                 Toast.makeText(getApplicationContext(),
