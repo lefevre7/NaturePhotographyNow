dc11d9715d69a0a3f8904ab7b1eab200b39119eb
Jeremy <lefevre_jeremy@yahoo.com>
2015-07-16 14:26:34 -0600

    Events activity is now working, though the user has to type in the data

--- app/src/main/java/com/example/jeremy/naturephotographynow/activity/EventsActivity.java
+++ app/src/main/java/com/example/jeremy/naturephotographynow/activity/EventsActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
 
 import com.example.jeremy.naturephotographynow.R;
 
@@ -27,6 +29,13 @@ public class EventsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
+        String url = "http://www.naturephotographynow.com/#/page/events/";
+        WebView webview = (WebView) this.findViewById(R.id.webView2);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setBuiltInZoomControls(true);
+        webview.loadUrl(url);
     }
 
     @Override
@@ -54,11 +63,38 @@ public class EventsActivity extends ActionBarActivity {
         public void handleLoadFilesClick(View view){
             Intent calIntent = new Intent(Intent.ACTION_INSERT);
             calIntent.setType("vnd.android.cursor.item/event");
-            calIntent.putExtra(CalendarContract.Events.TITLE, "My House Party");
-            calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Beach House");
-            calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "A Pig Roast on the Beach");
 
-            GregorianCalendar calDate = new GregorianCalendar(2012, 7, 15);
+            EditText editText1 = (EditText) findViewById(R.id.Title);
+            EditText editText2 = (EditText) findViewById(R.id.Location);
+            EditText editText3 = (EditText) findViewById(R.id.Description);
+            EditText editText4 = (EditText) findViewById(R.id.Year);
+            EditText editText5 = (EditText) findViewById(R.id.Month);
+            EditText editText6 = (EditText) findViewById(R.id.Day);
+
+            String title = editText1.getText().toString();
+            String loc = editText2.getText().toString();
+            String desc = editText3.getText().toString();
+
+            String yr = editText4.getText().toString();
+            String mo = editText5.getText().toString();
+            String da = editText6.getText().toString();
+
+            Integer yr1 = Integer.parseInt(yr);
+            Integer mo1 = Integer.parseInt(mo) - 1;//month "0" is January, not "1"
+            Integer da1 = Integer.parseInt(da);
+
+
+
+            //intent.putExtra("theBook", book);
+            //intent.putExtra("theChapter", chapter);
+            //intent.putExtra("theVerse", verse);
+            //startActivity(intent);
+
+            calIntent.putExtra(CalendarContract.Events.TITLE, title);
+            calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, loc);
+            calIntent.putExtra(CalendarContract.Events.DESCRIPTION, desc);
+
+            GregorianCalendar calDate = new GregorianCalendar(yr1, mo1, da1);
             calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
             calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
                     calDate.getTimeInMillis());
